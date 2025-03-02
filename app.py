f498971cad8a09d1a6f82099e0ca3df9f3f669e1
trh4ckn0n <trhacknon@proton.me>
2025-03-02 02:35:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,14 @@ from database import db, Signalement
 from scraper import get_suspected_scammers
 
 app = Flask(__name__)
-
-# Configuration de la base de données
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'supersecret'
-
-# Initialisation de la base de données
 db.init_app(app)
 
 @app.route('/')
 def index():
-    # Récupérer les signalements de la base de données
     signalements = Signalement.query.all()
-    # Récupérer les arnaqueurs du web
-    escrocs_web = get_suspected_scammers()
+    escrocs_web = get_suspected_scammers()  # Récupération des arnaqueurs du web
     return render_template('index.html', signalements=signalements, escrocs_web=escrocs_web)
 
 @app.route('/signaler', methods=['GET', 'POST'])
@@ -27,7 +21,6 @@ def signaler():
         telephone = request.form['telephone']
         description = request.form['description']
 
-        # Ajouter le nouveau signalement à la base de données
         nouveau_signalement = Signalement(nom=nom, email=email, telephone=telephone, description=description)
         db.session.add(nouveau_signalement)
         db.session.commit()
@@ -37,9 +30,6 @@ def signaler():
     return render_template('signaler.html')
 
 if __name__ == '__main__':
-    # Créer les tables de la base de données
     with app.app_context():
         db.create_all()
-
-    # Lancer l'application Flask
     app.run(debug=False, host="0.0.0.0")
