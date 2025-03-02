7cafa85fc189bcdd785c44b44bcb34126b9dad79
trh4ckn0n <trhacknon@proton.me>
2025-03-02 01:37:37 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+from database import db, Signalement
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'supersecret'
+db.init_app(app)
+
+@app.route('/')
+def index():
+    signalements = Signalement.query.all()
+    return render_template('index.html', signalements=signalements)
+
+@app.route('/signaler', methods=['GET', 'POST'])
+def signaler():
+    if request.method == 'POST':
+        nom = request.form['nom']
+        email = request.form['email']
+        telephone = request.form['telephone']
+        description = request.form['description']
+        
+        nouveau_signalement = Signalement(nom=nom, email=email, telephone=telephone, description=description)
+        db.session.add(nouveau_signalement)
+        db.session.commit()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('signaler.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=False, host="0.0.0.0")
