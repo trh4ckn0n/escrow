91b46fc6c5b2f3c2f0ebda659f49bde9a47cbf26
trh4ckn0n <trhacknon@proton.me>
2025-03-02 02:22:10 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
-# database.py
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialisation de SQLAlchemy
 db = SQLAlchemy()
 
 class Signalement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=True)
-    telephone = db.Column(db.String(20), unique=True, nullable=True)
+    email = db.Column(db.String(100), nullable=False)
+    telephone = db.Column(db.String(20), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    date_signalement = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f'<Signalement {self.nom}>'
